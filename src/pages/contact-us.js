5329a77a9000833bd9b24f2e1eac0344d64a0c09
Kumar Ankur <akakankur81@gmail.com>
2020-08-01 17:32:16 +0530

    comment incorporated

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -59,123 +59,125 @@ export default function ContactUs() {
   }
 
   return (
-    <div className="wrapper">
-      <div className="contact">
-        <div className="heading">
-          <h2 className="title">
-            <span className="block">
-              <span className="lineContainer">
-                <span style={{ transform: "translate(0px, 0%)" }}>
-                  <font style={{ verticalAlign: "inherit" }}>{constants.TITLE}</font>
+    <div className="contactus">
+      <div className="wrapper">
+        <div className="contact">
+          <div className="heading">
+            <h2 className="title">
+              <span className="block">
+                <span className="lineContainer">
+                  <span style={{ transform: "translate(0px, 0%)" }}>
+                    <font style={{ verticalAlign: "inherit" }}>{constants.TITLE}</font>
+                  </span>
+                </span>
+                <span className="lineContainer">
+                  <span style={{ transform: "translate(0px, 0%)" }}>
+                    <font className="fontStyle">{constants.SUB_TITILE}</font>
+                  </span>
                 </span>
               </span>
-              <span className="lineContainer">
-                <span style={{ transform: "translate(0px, 0%)" }}>
-                  <font className="fontStyle">{constants.SUB_TITILE}</font>
-                </span>
-              </span>
-            </span>
-          </h2>
-        </div>
-
-        <div className="contactContainer">
-          <div className="formContainer">
-            <form className="formGen" name="contact" action="POST" data-netlify="true">
-              <TextField
-                id="standard-basic"
-                label="First Name"
-                name="First Name"
-                className="inputType"
-                style={{ marginBottom: "2rem" }}
-                onChange={(e) => setFname(e.target.value)}
-              />
-              <TextField
-                id="standard-basic"
-                label="Last Name"
-                name="Last Name"
-                className="inputType"
-                onChange={(e) => setLname(e.target.value)}
-              />
-              <TextField
-                id="standard-basic"
-                label="Email"
-                className="inputType"
-                name="Email"
-                style={{ marginBottom: "2rem" }}
-                onChange={(e) => emailValidator(e)}
-                helperText={!isValidEmail ? "Incorrect Email Address" : ""}
-              />
-              <TextField
-                id="standard-basic"
-                label="Phone Number"
-                name="Phone"
-                className="inputType"
-                onChange={(e) => phoneValidator(e)}
-                helperText={!isValidPhone ? "Incorrect Phone Number" : ""}
-              />
-              <TextField
-                id="standard-basic"
-                label="Message"
-                name="Message"
-                className="inputType"
-                style={{ width: "100%", marginBottom: "2rem" }}
-                onChange={(e) => setMessage(e.target.value)}
-              />
-
-              <RadioGroup
-                aria-label="terms"
-                name="terms"
-                value={value}
-                onChange={handleChange}
-                style={{ marginTop: "2rem" }}
-              >
-                <FormControlLabel
-                  value="terms"
-                  style={{ color: "#717171", fontFamily: "IBM Plex Sans Condensed, sans-serif" }}
-                  control={<Radio />}
-                  label={constants.TERMS_CONDITION}
-                />
-              </RadioGroup>
-
-              <div className="main">
-                <div className="sub-main">
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    endIcon={<SendIcon />}
-                    disabled={isSendDisabled}
-                    style={{
-                      backgroundColor: "#05a8aa",
-                    }}
-                  >
-                    {constants.SEND}
-                  </Button>
-                </div>
-              </div>
-            </form>
+            </h2>
           </div>
 
-          <div className="info">
-            <ul style={{ listStyle: "none" }}>
-              <h4 className="heading">{constants.INFO_HEADING}</h4>
-              <li className="listStyle">
-                {constants.CONTACT}
-                <a style={{ marginLeft: ".6rem", color: "#717171" }} href="mailto:heraclesteam2020@gmail.com">
-                  {constants.SUPPORT_EMAIL}
-                </a>
-              </li>
-            </ul>
+          <div className="contactContainer">
+            <div className="formContainer">
+              <form className="formGen" name="contact" action="POST" data-netlify="true">
+                <TextField
+                  id="standard-basic"
+                  label="First Name"
+                  name="First Name"
+                  className="inputType"
+                  style={{ marginBottom: "2rem" }}
+                  onChange={(e) => setFname(e.target.value)}
+                />
+                <TextField
+                  id="standard-basic"
+                  label="Last Name"
+                  name="Last Name"
+                  className="inputType"
+                  onChange={(e) => setLname(e.target.value)}
+                />
+                <TextField
+                  id="standard-basic"
+                  label="Email"
+                  className="inputType"
+                  name="Email"
+                  style={{ marginBottom: "2rem" }}
+                  onChange={(e) => emailValidator(e)}
+                  helperText={!isValidEmail ? "Incorrect Email Address" : ""}
+                />
+                <TextField
+                  id="standard-basic"
+                  label="Phone Number"
+                  name="Phone"
+                  className="inputType"
+                  onChange={(e) => phoneValidator(e)}
+                  helperText={!isValidPhone ? "Incorrect Phone Number" : ""}
+                />
+                <TextField
+                  id="standard-basic"
+                  label="Message"
+                  name="Message"
+                  className="inputType"
+                  style={{ width: "100%", marginBottom: "2rem" }}
+                  onChange={(e) => setMessage(e.target.value)}
+                />
 
-            <ul style={{ listStyle: "none", marginTop: "2rem" }}>
-              <h4 className="heading">{constants.PHONE}</h4>
-              <li className="listStyle">{constants.SUPPORT_NUMBER}</li>
-            </ul>
+                <RadioGroup
+                  aria-label="terms"
+                  name="terms"
+                  value={value}
+                  onChange={handleChange}
+                  style={{ marginTop: "2rem" }}
+                >
+                  <FormControlLabel
+                    value="terms"
+                    style={{ color: "#717171", fontFamily: "IBM Plex Sans Condensed, sans-serif" }}
+                    control={<Radio />}
+                    label={constants.TERMS_CONDITION}
+                  />
+                </RadioGroup>
 
-            <ul style={{ listStyle: "none", marginTop: "2rem" }}>
-              <h4 className="heading">{constants.OFFICE}</h4>
-              <address className="listStyle">{constants.ADDRESS_1}</address>
-              <address className="listStyle">{constants.ADDRESS_2}</address>
-            </ul>
+                <div className="main">
+                  <div className="sub-main">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      endIcon={<SendIcon />}
+                      disabled={isSendDisabled}
+                      style={{
+                        backgroundColor: "#05a8aa",
+                      }}
+                    >
+                      {constants.SEND}
+                    </Button>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            <div className="info">
+              <ul style={{ listStyle: "none" }}>
+                <h4 className="heading">{constants.INFO_HEADING}</h4>
+                <li className="listStyle">
+                  {constants.CONTACT}
+                  <a style={{ marginLeft: ".6rem", color: "#717171" }} href="mailto:heraclesteam2020@gmail.com">
+                    {constants.SUPPORT_EMAIL}
+                  </a>
+                </li>
+              </ul>
+
+              <ul style={{ listStyle: "none", marginTop: "2rem" }}>
+                <h4 className="heading">{constants.PHONE}</h4>
+                <li className="listStyle">{constants.SUPPORT_NUMBER}</li>
+              </ul>
+
+              <ul style={{ listStyle: "none", marginTop: "2rem" }}>
+                <h4 className="heading">{constants.OFFICE}</h4>
+                <address className="listStyle">{constants.ADDRESS_1}</address>
+                <address className="listStyle">{constants.ADDRESS_2}</address>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
